d5f9fcc90d479ff54482cc43465acec53db7fc4c
Giza25 <dyma125@gmail.com>
2025-10-10 15:00:36 +0400

    Added get_file_content() function that read a file and return it's contents Added config.py updated tests.py

---
+++ functions/get_files_content.py
@@ -0,0 +1,17 @@
+import os
+import config
+
+def get_files_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    abs_path = os.path.abspath(full_path)
+
+    if not abs_path.startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_path):
+        return f'Error: "{file_path}" is not a file'
+    
+    with open(abs_path, "r") as file:
+        file_content = file.read(config.MAX_CHARACTERS)
+    if len(file_content) == config.MAX_CHARACTERS:
+        file_content += f'[...File "{file_path}" truncated at {config.MAX_CHARACTERS} characters]'
+    return file_content
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import unittest
 from functions.get_files_info import get_files_info
+from functions.get_files_content import get_files_content
 
+"""
 class TestGetFilesInfo(unittest.TestCase):
     def test_default(self):
         print("\nResult for current directory:")
@@ -31,6 +33,33 @@ class TestGetFilesInfo(unittest.TestCase):
         result = get_files_info("calculator", "calculator")
         print(result)
         self.assertTrue('Error:' in result)
+"""
+
+class TestGetFilesContent(unittest.TestCase):
+    def test_main(self):
+        print(f'\nResult for "main.py" file:')
+        result = get_files_content("calculator", "main.py")
+        print(result)
+        self.assertTrue(f'def main():' in result)
+
+    def test_file_in_directory(self):
+        print(f'\nResult for "pkg/calculator.py" file:')
+        result = get_files_content("calculator", "pkg/calculator.py")
+        print(result)
+        self.assertTrue(f'class Calculator:' in result)
+
+    def test_directory(self):
+        print(f'\nResult for "/pkg" directory:')
+        result = get_files_content("calculator", "pkg")
+        print(result)
+        self.assertTrue(f'Error: ' in result)
+
+    def test_not_exists(self):
+        print(f'\nResult for "pkg/file_does_not_exist.py" file:')
+        result = get_files_content("calculator", "pkg/file_does_not_exist.py")
+        print(result)
+        self.assertTrue(f'Error: ' in result)
+
 
 if __name__ == "__main__":
     unittest.main()
